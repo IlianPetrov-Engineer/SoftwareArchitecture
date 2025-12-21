a3cfe9eb96f31b05fb00cc105b69a9a659c11412
Ilian Petrov <ilianpetrov.personal@gmail.com>
2025-12-21 14:00:34 +0200

    Restoration

---
+++ SoftwareArchitecture/Assets/Scipts/ItemData.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CMGTSA.Inventory
+{
+    /// <summary>
+    /// This is the script for creating an ItemData scriptable object, which is
+    /// "blueprint" to create item objects with the properties set up in the inspector,
+    /// it is an implementation of factory pattern.defenseite
+    /// </summary>
+    [CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
+    public class ItemData : ScriptableObject
+    {
+        [Header("Unique id for each item")]
+        public string id;
+
+        [Header("Core properties")]
+        public string itemName;
+        public int health;
+        public int defense;
+        public int value;
+        public float buffPercentage;
+       // public bool isPotion;
+
+        [Header("Visuals")]
+        public Sprite itemIcon;
+        public GameObject itemModel;
+
+        public Item CreateItem()
+        {
+            return new Item(this);
+        }
+    }
+
+    [Serializable]
+    public class Item
+    {
+        [Header("Unique id for each item")]
+        [SerializeField]
+        private string id;
+        public string Id => id;//This setup allows access to the private field 'id'
+                               //while also allows it to be shown in the inspector
+
+        [Header("Core properties")]
+        [SerializeField]
+        private string itemName;
+        public string ItemName => itemName;
+        [SerializeField]
+        private int health;
+        public int Health => health;
+        [SerializeField]
+        private int defense;
+        public int Defense => defense;
+        [SerializeField]
+        private int value;
+        public int Value => value;
+        [SerializeField]
+        private float buffPercentage;
+        public float BuffPercentage => buffPercentage;
+
+        //[SerializeField]
+        //private bool isPotion;
+        //public bool IsPotion => isPotion;
+
+        [Header("Visuals")]
+        public Sprite itemIcon;
+        public GameObject itemModel;
+
+        public Item(ItemData itemData)
+        {
+            id = itemData.id;
+            itemName = itemData.itemName;
+            health = itemData.health;
+            defense = itemData.defense;
+            value = itemData.value;
+            buffPercentage = itemData.buffPercentage;
+
+            itemIcon = itemData.itemIcon;
+            itemModel = itemData.itemModel;
+            //isPotion = itemData.isPotion;
+        }
+    }
+}
---
+++ SoftwareArchitecture/Assets/Scipts/ListViewItemPresenter.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CMGTSA.Inventory
+{
+    /// <summary>
+    /// This class presents an item in a list in the GUI.
+    /// </summary>
+    public class ListViewItemPresenter : ItemPresenter
+    {
+        public Image icon;
+        public TextMeshProUGUI nameText;
+        public TextMeshProUGUI attackText;
+        public TextMeshProUGUI defenseText;
+
+        public override void PresentItem(Item item)
+        {
+            icon.sprite = item.itemIcon;
+            nameText.text = item.ItemName;
+            if (item.Health != 0)
+                attackText.text = item.Health.ToString();
+            else
+                attackText.text = "";
+            if (item.Defense != 0)
+                defenseText.text = item.Defense.ToString();
+            else
+                defenseText.text = "";
+        }
+    }
+}
